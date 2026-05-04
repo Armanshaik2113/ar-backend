eaa30255576e0b48eafe25bb93e4ddfe7f079c42
ArmanShaik2113 <armanshaik5120@gmail.com>
2026-05-04 09:47:03 +0530

    prepare for deployment

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ app.post("/contact", async (req, res) => {
     res.status(500).json({ success: false });
   }
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5501, () => {
-  console.log("🔥 Running on http://127.0.0.1:5501");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
